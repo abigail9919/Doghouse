c881835ca88a6e5e2c904069ca004c6e275aafef
Abby <ard99@yahoo.com>
2023-11-13 07:59:02 -0500

    finished 1-3

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,32 @@ const Dog = mongoose.model('dogs')
 
 /* GET home page. */
 router.get('/', async function(req, res) {
-  console.log("-- yes here i am --");
   const filter = {};
   const dogs = await Dog.find();
-  console.log("DOGS: " + dogs);
   res.render('index', { title: 'DogHouse', dogs: dogs });
+});
+
+/* GET new dog form page. */
+router.get('/newdog', async function(req, res) {
+  res.render('newdog', { title: 'New Dog Form' });
+});
+
+/* POST a new dog form submission */
+router.post('/', async (req, res) => {
+  console.log(req.body);  
+  
+  const myDog = new Dog({
+    name: req.body.name,
+    gender: req.body.gender,
+    breed: req.body.breed,
+    age: req.body.age,
+    treat: req.body.treat
+  });
+  myDog.save()
+    .then( () => console.log('Document saved.') )
+    // .then( () => { res.render('index', { title: 'Form posted' }); })
+    .then( () => { res.redirect('/') })
+    .catch( err => console.log(err) );
 });
 
 module.exports = router;
